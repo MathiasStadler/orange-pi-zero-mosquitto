f30fb0d74179367cc3ab63b72587d9f1035519f9
trapapa <stadler-mathias@web.de>
2019-03-08 16:51:42 +0100

    chn to 127.0.0.1

--- subscribe_simple.py
+++ subscribe_simple.py
@@ -24,7 +24,7 @@ import paho.mqtt.subscribe as subscribe
 
 topics = ['#']
 
-m = subscribe.simple(topics, hostname="iot.eclipse.org", retained=False, msg_count=2)
+m = subscribe.simple(topics, hostname="127.0.0.1", retained=False, msg_count=2)
 for a in m:
     print(a.topic)
 print(a.payload)
